7d8f3ed5997a2e26fc146a0bdc337f9a33ce8eb6
ChingHannie <ching.hannie@dhs.sg>
2015-01-31 21:16:09 +0800

    Update q04_determine_leap_year.py

--- p02/q04_determine_leap_year.py
+++ p02/q04_determine_leap_year.py
@@ -3,7 +3,9 @@ __author__ = 'Hannie'
 year = int(input("Enter year: "))
 if year % 400 == 0:
     print("Leap")
-elif year % 4 == 0 and year % 100 != 0:
+elif year % 4 == 0:
     print("Leap")
+elif year % 100 != 0:
+    print("Non-Leap")
 else:
     print("Non-Leap")
