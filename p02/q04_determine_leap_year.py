a236714541af3b58fe642ea34e2be86f91f91249
ChingHannie <ching.hannie@dhs.sg>
2015-01-31 21:23:28 +0800

    Update q04_determine_leap_year.py

--- p02/q04_determine_leap_year.py
+++ p02/q04_determine_leap_year.py
@@ -1,11 +1,8 @@
 __author__ = 'Hannie'
 
 year = int(input("Enter year: "))
-if year % 400 == 0:
+import calendar
+if calendar.isleap(year) == True:
     print("Leap")
-elif year % 4 == 0:
-    print("Leap")
-elif year % 100 != 0:
-    print("Non-Leap")
 else:
     print("Non-Leap")
